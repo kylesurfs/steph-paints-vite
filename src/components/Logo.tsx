bd83235c355934eed286e5dd2e996e73e1edbb94
Kyle Reagan <kylereagan37@gmail.com>
2023-12-07 18:43:35 -0800

    Updated Logo component to have more info about Steph

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,15 @@
 export default function Logo() {
   return (
     <>
-      <div className='font-bold text-3xl mx-auto max-w-2xl py-2 sm:px-6 lg:max-w-7xl lg:px-8 text-black dark:text-white'>
-        <h1>Steph Paints</h1>
+      <div className='mx-auto max-w-2xl pt-10 pb-4 sm:px-6 lg:max-w-7xl lg:px-8 text-black dark:text-white'>
+        <h1 className='font-bold text-5xl'>StephPaints</h1>
+        <p className='mt-6 text-base text-zinc-600 dark:text-zinc-400'>
+          I’m Steph, an artist, surfer, and non-profit consultant from San
+          Clemente, California. This site is where I keep a record of my latest
+          projects -- both completed and active. If you are interested in having
+          me create something for you or someone you know, then please reach out
+          through the "Contact" page.
+        </p>
       </div>
     </>
   );
