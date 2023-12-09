276369866b2729d149a6f09f4c8f9d9fde44a11b
Kyle Reagan <kylereagan37@gmail.com>
2023-12-09 14:07:35 -0800

    Updated formatting on Home page to remove placeholder text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Cards from '../components/Cards';
-import FilterBar from '../components/FilterBar';
 import SearchBar from '../components/SearchBar';
 import Logo from '../components/Logo';
 
@@ -10,7 +9,6 @@ const Home: React.FC = () => {
   return (
     <>
       <div className='dark:bg-zinc-900'>
-        <FilterBar />
         <Logo />
         <SearchBar onSearch={setSearchTerm} />
         <Cards searchTerm={searchTerm} />
