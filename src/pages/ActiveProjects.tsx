6fb761512d4bef737dde6a7e310b6ea88ba38409
Kyle Reagan <kylereagan37@gmail.com>
2023-12-09 12:49:41 -0800

    Updated the format of Active Projects to remove placeholder text

--- src/pages/ActiveProjects.tsx
+++ src/pages/ActiveProjects.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Cards from '../components/Cards';
-import FilterBar from '../components/FilterBar';
 import SearchBar from '../components/SearchBar';
 
 const ActiveProjects: React.FC = () => {
@@ -8,10 +7,7 @@ const ActiveProjects: React.FC = () => {
 
   return (
     <>
-      <div className='dark:bg-zinc-900'>
-        <h1>This is the actibe paintings page. It needs to be updated.</h1>
-        {/* DEV -- delete above */}
-        <FilterBar />
+      <div className='dark:bg-zinc-900 py-8'>
         <SearchBar onSearch={setSearchTerm} />
         <Cards searchTerm={searchTerm} />
       </div>
