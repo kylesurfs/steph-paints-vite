4c6359064476384a91207ff29ddb2d921a2746d9
Kyle Reagan <kylereagan37@gmail.com>
2024-01-23 21:04:42 -0800

    Updated notFoundPage to use S3 image instead of including in bundle

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,11 +1,36 @@
 //== react, react-router-dom, Auth0 ==//
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-//== TSX Components, Functions ==//
-import notFoundImage from '../images/notFoundImage.png';
+//-- NPM imports --//
+import axios from 'axios';
+import { square } from 'ldrs'; //-- Custom loader imported from ldrs (https://uiball.com/ldrs/) --//
+square.register();
 
 //== ***** ***** ***** Exported Component ***** ***** ***** ==//
 export default function NotFoundPage() {
+  const [imageSrc, setImageSrc] = useState<string | undefined>();
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
+
+  //-- Fetch image of randomAnimal from S3 bucket --//
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await axios.get(
+          'https://steph-paints-art-pics.s3.amazonaws.com/notFoundImage.jpeg',
+          {
+            responseType: 'blob',
+          }
+        );
+        const imageBlob: Blob = await response.data;
+        const imageURL: string = URL.createObjectURL(imageBlob);
+        setImageSrc(imageURL);
+        setImageLoaded(true);
+      } catch (err) {}
+    };
+    fetchImage();
+  }, []);
+
   //== Handlers ==//
   const handleClick = () => {
     window.scrollTo(0, 0); // Scroll to the top of the page
@@ -23,12 +48,20 @@ export default function NotFoundPage() {
         ```
       */}
       <main className='relative isolate h-screen'>
-        <img
-          //   src='https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75'
-          src={notFoundImage}
-          alt=''
-          className='absolute inset-0 -z-10 h-full w-full object-cover object-top'
-        />
+        {imageLoaded ? (
+          <>
+            <img
+              src={imageSrc}
+              alt=''
+              className='absolute inset-0 -z-10 h-full w-full object-cover object-top'
+            />
+          </>
+        ) : (
+          //insert loader from form submission
+          <div className='flex justify-center items-center'>
+            <l-square color='teal'></l-square>
+          </div>
+        )}
         <div className='mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8'>
           <p className='text-base font-semibold leading-8 text-white'>404</p>
           <h1 className='mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl'>
